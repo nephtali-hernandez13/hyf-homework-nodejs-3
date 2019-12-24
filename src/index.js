9b7d1efe4c0e1b49b6771c369d67e3201b48b034
nephtali-hernandez13 <58494407+nephtali-hernandez13@users.noreply.github.com>
2019-12-24 13:58:20 -0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -6,8 +6,8 @@ let list = []
 app.get('/', (req, res) => res.send('Hello World!'))
 app.get('/users', (req,res) => { res.json( list ) })
 app.post('/user', (req,res) => {         
-    //list.push({id: 0})
-    list.push({ id: ((list.length+1)-1) }); res.json(list) 
+    list.push({id: 0})
+    //list.push({ id: ((list.length+1)-1) }); res.json(list) 
     res.json(list);
 })
 
