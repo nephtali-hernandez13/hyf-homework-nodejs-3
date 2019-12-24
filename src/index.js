6036c0bde802a75a2377f4928bf47593e0e1bb73
nephtali-hernandez13 <58494407+nephtali-hernandez13@users.noreply.github.com>
2019-12-24 13:42:34 -0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -6,6 +6,6 @@ let list = []
 app.get('/', (req, res) => res.send('Hello World!'))
 app.get('/users', (req,res) => { res.json( list ) })
 app.get('/user/:id', (req,res) => res.send('User'))
-app.post('user', (req,res) => res.send('POST request'))
+app.post('user', (req,res) => { list.push({id:0}); res.json(list) })
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
