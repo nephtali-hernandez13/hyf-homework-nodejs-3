735835b5bcc4b5324a12d2799b1b8578b7ea107e
nephtali-hernandez13 <58494407+nephtali-hernandez13@users.noreply.github.com>
2019-12-25 21:54:18 -0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,20 +1,26 @@
-const express = require('express')
-const app = express()
-const port = 3000
-let list = []
+const express = require('express');
+const app = express();
+const port = 3000;
+let list = [];
 
-app.get('/', (req, res) => res.send('Hello World!'))
-app.get('/users', (req,res) => { res.json( list ) })
+app.get('/', (req, res) => res.send('Hello World!'));
+app.get('/users', (req,res) =>  res.json( list ));
 
 app.get('/user/:id', (req,res) => {     
-    const user = list.find(usr => usr.id == req.params.id)
-    res.json(user)
-})
+    const user = list.find(usr => usr.id == req.params.id);
+    res.json(user);
+});
 
 app.post('/user', (req,res) => {         
-    list.push({id: 0})
+    list.push({id: 0});
     //list.push({ id: ((list.length+1)-1) }); res.json(list) 
     res.json(list);
-})
+});
+
+app.delete('/user/:id', (req, res) => {
+    index = list.indexOf(req.params.id);
+    list.splice(index,1);
+    res.json(list);
+});
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
