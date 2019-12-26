848be0a6bb332fb682302d20ab26f3e2e696b4bd
nephtali-hernandez13 <58494407+nephtali-hernandez13@users.noreply.github.com>
2019-12-25 21:41:08 -0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -5,6 +5,12 @@ let list = []
 
 app.get('/', (req, res) => res.send('Hello World!'))
 app.get('/users', (req,res) => { res.json( list ) })
+
+app.get('/user/:id', (req,res) => {     
+    const user = list.find(usr => usr.id == req.params.id)
+    res.json(user)
+})
+
 app.post('/user', (req,res) => {         
     list.push({id: 0})
     //list.push({ id: ((list.length+1)-1) }); res.json(list) 
